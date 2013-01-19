f4f46eb4830f9fa2d1d8540d795a37ae000edd86
yangjiandong <young.jiandong@gmail.com>
2013-01-19 22:40:30 +0800

    utf8

--- src/TestPyDev.py
+++ src/TestPyDev.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 # Default include for SL4A
 import android
 droid = android.Android()
 
-droid.makeToast('Hello,ddd0000')
+droid.makeToast('Hello,ddd0000,ZW,中文倪')
 print "Hello"
